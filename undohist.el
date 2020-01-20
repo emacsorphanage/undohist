6c905772e6aa9969fd9ca500d2fe93e114130fe6
jenchieh <jcs090218@gmail.com>
2020-01-20 21:28:28 +0800

    Bump version.

--- undohist.el
+++ undohist.el
@@ -5,7 +5,7 @@
 ;; Author: MATSUYAMA Tomohiro <m2ym.pub@gmail.com>
 ;; Package-Requires: ((cl-lib "1.0"))
 ;; Keywords: convenience
-;; Version: 0.2
+;; Version: 0.2.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
